adc10283cc29856d50210632bcfbb621871bef34
Engin Bolat <engi.bolat@gmail.com>
2024-07-13 19:39:26 +0300

    refactor: yarn install

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,31 +1,33 @@
-import { useState, useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import { multiply, PrimaryButton } from 'engin-ui';
+import { StyleSheet, View } from 'react-native';
+import { Button, TextInput } from 'engin-ui';
 
-export default function App() {
-  const [result, setResult] = useState<number | undefined>();
-
-  useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
+const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
-      <PrimaryButton onPress={() => {}} title="Click" disabled={false} />
+      <TextInput
+        value="value"
+        maxLength={200}
+        multiline={true}
+        showMaxLengthTextAlign="right"
+      />
+      <Button
+        onPress={() => {}}
+        title="Button"
+        width="full"
+        disabled={false}
+        isHaveShadow={false}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+    padding: 20,
   },
 });
+
+export default App;
